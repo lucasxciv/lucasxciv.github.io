46acd843c0093934c884516ad74b23fa89fc0271
Lucas de Oliveira <94oliveira.lucas@gmail.com>
2018-05-23 21:47:00 -0300

    Add post not found handler

--- public/index.php
+++ public/index.php
@@ -2,10 +2,12 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
+use Blog\Exception;
 use Blog\GetPost;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
+use Slim\Exception\NotFoundException;
 
 (new Dotenv\Dotenv(__DIR__ . '/..'))->load();
 
@@ -26,9 +28,13 @@ $app->get('/blog/{slug}', function (Request $request, Response $response, array 
     /** @var GetPost $getPost */
     $getPost = $this->get(GetPost::class);
 
-    return $this->view->render($response, 'post.html.twig', [
-        'post' => $getPost->oneBySlug($args['slug'])
-    ]);
+    try {
+        $post = $getPost->oneBySlug($args['slug']);
+    } catch (Exception\PostNotFound $e) {
+        throw new NotFoundException($request, $response);
+    }
+
+    return $this->view->render($response, 'post.html.twig', [ 'post' => $post ]);
 });
 
 $app->run();
