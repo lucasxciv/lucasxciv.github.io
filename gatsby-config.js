de937121a6b54cd5f7fe070dec5d838d397e14e7
Lucas de Oliveira <contato@deoliveiralucas.net>
2020-06-27 17:44:25 -0300

    add netlify cms config

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,6 +91,7 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify-cms`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
