e3510811dcab4845393447d8571146caa3a5b3af
Lucas de Oliveira <mail@lucasxciv.dev>
2024-10-03 17:38:23 -0300

    Adjust about page

--- source/about.blade.php
+++ source/about.blade.php
@@ -19,7 +19,7 @@ description: A little bit about the site
 
     <p class="mb-6 text-base01">As a software developer primarily working on web applications and APIs, I've often found myself working on other parts of the product as well. I've worked on frontend tasks using SPA frameworks, set up continuous integration pipelines with cloud tools, contributed to mobile and desktop apps, helped with cloud platform deployments, built automation tools, led development teams, etc.</p>
 
-    <p class="mb-6 text-base01">I’ve been working fully remotely since 2018, and since 2021, I’ve been collaborating with international teams, communicating asynchronously across different time zones. I’ve also contributed to open-source projects, gaining valuable experience in community-driven development.</p>
+    <p class="mb-6 text-base01">I've been working fully remotely since 2018, and since 2021, I've been collaborating with international teams, communicating asynchronously across different time zones. I've also contributed to open-source projects, gaining valuable experience in community-driven development. Speaking of community, from 2018 to 2020, I co-organized the <a href="https://phpvale.github.io/" target="_blank" class="text-base01">PHPVale User Group</a> meetups in my local area.</p>
 
     <p class="mb-6 text-base01">That's a bit about me as a software developer. I'm constantly learning and improving my skills, and I really enjoy building software that solves real-world problems and helps people streamline their tasks. If you'd like to get in touch, feel free to connect with me on any of my social media platforms, or you can email me at <a href="mailto:mail@lucasxciv.dev" class="text-base01">mail@lucasxciv.dev</a>.</p>
 @endsection
