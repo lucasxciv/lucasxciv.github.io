7eb79c3bb2d7158bb00844ddca218c5e8ca8fa2a
Lucas de Oliveira <mail@lucasxciv.dev>
2024-10-03 17:05:23 -0300

    Add about page

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,15 +5,21 @@ description: A little bit about the site
 @extends('_layouts.main')
 
 @section('body')
-    <h1>About</h1>
+    <h1>About me</h1>
 
-    <img src="/assets/img/about.png"
-        alt="About image"
-        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+    <img src="/assets/img/about-me.jpg"
+        alt="About me"
+        class="flex rounded-lg border-4 border-base2 shadow w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
+    <p class="mb-6 text-base01">I'm Lucas de Oliveira (<b>@lucasxciv</b> on most social media platforms), a software developer since 2013. Over the years, I've worked in various areas of software development, but I've primarily worked with backend tasks using PHP. Currently, I work as an independent software developer and consultant, based in Cruzeiro, São Paulo, Brazil.</p>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
+    <p class="mb-6 text-base01">I began formally studying programming in 2011 through a technical computing course at ETEC. In 2012, I started my degree in Information Technology with an emphasis in databases at FATEC, where I delved into topics like data structures, algorithms, advanced database systems, data mining, neural networks, and more.</p>
 
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+    <p class="mb-6 text-base01">In 2013, while still in university, I had the chance to start my career in the software industry as an intern working on an intranet system for a big company. That was my first real job using PHP. In 2015, I joined a post-graduation in web and mobile development, where I got to work with some of the latest technologies and build a great professional network. That same year, I also earned the <a href="https://drive.google.com/file/d/1OpMq1lSBVoqMt_G0MlBg5hprxHvZdvo-/view" target="_blank" class="text-base01">Zend Certified PHP Engineer</a> certification.</p>
+
+    <p class="mb-6 text-base01">As a software developer primarily working on web applications and APIs, I've often found myself working on other parts of the product as well. I've worked on frontend tasks using SPA frameworks, set up continuous integration pipelines with cloud tools, contributed to mobile and desktop apps, helped with cloud platform deployments, built automation tools, led development teams, etc.</p>
+
+    <p class="mb-6 text-base01">I’ve been working fully remotely since 2018, and since 2021, I’ve been collaborating with international teams, communicating asynchronously across different time zones. I’ve also contributed to open-source projects, gaining valuable experience in community-driven development.</p>
+
+    <p class="mb-6 text-base01">That's a bit about me as a software developer. I'm constantly learning and improving my skills, and I really enjoy building software that solves real-world problems and helps people streamline their tasks. If you'd like to get in touch, feel free to connect with me on any of my social media platforms, or you can email me at <a href="mailto:mail@lucasxciv.dev" class="text-base01">mail@lucasxciv.dev</a>.</p>
 @endsection
